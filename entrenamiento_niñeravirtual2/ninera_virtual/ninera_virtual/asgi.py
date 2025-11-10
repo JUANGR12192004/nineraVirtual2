b2ad159d91b5aef8f1e1955a7e532d9510504d87
JUANGR12192004 <jgutierrezr11@ucentral.edu.co>
2025-11-09 21:36:39 -0500

    v21.2

--- entrenamiento_niñeravirtual2/ninera_virtual/ninera_virtual/asgi.py
+++ entrenamiento_niñeravirtual2/ninera_virtual/ninera_virtual/asgi.py
@@ -1,26 +1,27 @@
 import os
+import logging
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ninera_virtual.settings")
 
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+
+# Importar rutas de WS de forma tolerante; si fallan, usar lista vacía sin romper ASGI
 try:
-    from channels.routing import ProtocolTypeRouter, URLRouter
-    from channels.auth import AuthMiddlewareStack
-    from channels.security.websocket import AllowedHostsOriginValidator
-    from django.core.asgi import get_asgi_application
-    from .routing import websocket_urlpatterns
-
-    django_asgi_app = get_asgi_application()
-    application = ProtocolTypeRouter(
-        {
-            "http": django_asgi_app,
-            # Valida Host en el handshake y aplica sesiones/cookies
-            "websocket": AllowedHostsOriginValidator(
-                AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
-            ),
-        }
-    )
+    from .routing import websocket_urlpatterns  # type: ignore
 except Exception:
-    # Fallback sin Channels
-    from django.core.asgi import get_asgi_application
+    logging.exception("[asgi] No se pudieron importar rutas WS; usando lista vacía")
+    websocket_urlpatterns = []  # type: ignore
 
-    application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
+    }
+)
